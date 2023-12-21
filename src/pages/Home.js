b0afb01b3fc058210221e83c03b67a9b8f974094
Dev <devendrapkmishra@gmail.com>
2023-12-21 22:12:37 +0530

    1st Production Error Fix Commit By Wrapping it Using Callback

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState, useCallback } from 'react';
 import { Box } from '@mui/material';
 
 import Exercises from '../components/Exercises';
@@ -6,16 +6,20 @@ import SearchExercises from '../components/SearchExercises';
 import HeroBanner from '../components/HeroBanner';
 
 const Home = () => {
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   const [exercises, setExercises] = useState([]);
   const [bodyPart, setBodyPart] = useState('all');
 
+  // Wrapping The setExercise to Fix This Error
+  // Error = `Line 27:6:  React Hook useEffect has a missing dependency: "setExercises". Either include it or remove the dependency array. If "setExercises" changes too often, find the parent component that defines it and wrap that definition in useCallback`
+  const memoizedSetExercises = useCallback((newExercises) => {
+    setExercises(newExercises);
+  }, []); 
 
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
+      <SearchExercises setExercises={memoizedSetExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={memoizedSetExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
   );
 };
