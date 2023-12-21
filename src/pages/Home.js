20058bbac92b0452af9fd1885a6b128cbf3b4d13
Dev <devendrapkmishra@gmail.com>
2023-12-21 22:26:28 +0530

    1st Commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState } from 'react';
 import { Box } from '@mui/material';
 
 import Exercises from '../components/Exercises';
@@ -9,17 +9,11 @@ const Home = () => {
   const [exercises, setExercises] = useState([]);
   const [bodyPart, setBodyPart] = useState('all');
 
-  // Wrapping The setExercise to Fix This Error
-  // Error = `Line 27:6:  React Hook useEffect has a missing dependency: "setExercises". Either include it or remove the dependency array. If "setExercises" changes too often, find the parent component that defines it and wrap that definition in useCallback`
-  const memoizedSetExercises = useCallback((newExercises) => {
-    setExercises(newExercises);
-  }, []); 
-
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises setExercises={memoizedSetExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-      <Exercises setExercises={memoizedSetExercises} exercises={exercises} bodyPart={bodyPart} />
+      <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} />
     </Box>
   );
 };
